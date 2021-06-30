9a26cef4ac5477fb2f1775a73246846dcafe27a7
Raza <77836119+Ahmedlathiya@users.noreply.github.com>
2021-06-30 18:46:41 +0500

    Nested Object
    Mobile Names And Details

---
+++ object Assignment/app.js
@@ -0,0 +1,135 @@
+let mobile = {
+
+    mortolla: {
+
+        mobile_Model_1: "Moto Z Play",
+        Build: {
+            OS: "Android 6.0.1 (Marshmallow), 7.1.1 (Nougat), planned upgrade to Android 8.0 (Oreo)",
+            Dimensions: "156.4 x 76.4 x 7 mm",
+            Weight: "165 g",
+            SIM: "Dual Sim, Dual Standby (Nano-SIM)",
+            mobile_Color: ["Black/Silver", "Black Slate", "White / Fine Gold", "Sugar White"],
+        },
+        Processor: {
+            CPU: "2.0 GHZ Octa-Core Cortex A53",
+            Chipset: "Qualcomm MSM8953 Snapdragon 625",
+            GPU: "Adreno 506",
+        },
+        Display: {
+            Size: "5.5 Inches",
+            Protection: "Corning Gorilla Glass 3",
+        },
+        price: "Rs : 40,000",
+
+
+        mobile_Model_2: "motorola Moto G6 Plus",
+        Build: {
+            OS: "Android v7.1.1 Nougat",
+            Dimensions: "160 x 75.5 x 8 mm",
+            Weight: "167 g",
+            SIM: "Dual Sim, Dual Standby (Nano-SIM)",
+            Colors: "Gold, Deep Indigo, Nimbus",
+        },
+        Processor: {
+            CPU: "2.2 Ghz Octa Core Cortex-A53",
+            Chipset: "Qualcomm SDM630 Snapdragon 630",
+            GPU: " Adreno 508",
+        },
+        Display: {
+            Size: "5.5 Inches",
+            Protection: "Corning Gorilla Glass 3",
+        },
+        price: "Rs : 42,000",
+    },
+
+    samsung: {
+
+
+        mobile_Model_1: "Samsung Galaxy Z Fold",
+        Build: {
+            OS: "Android 10 OS",
+            UI: "One UI 2.1",
+            Dimensions: "Unfolded: (159.2 x 128.2 x 6.9 mm), Folded: (159.2 x 68 x 16.8 mm)",
+            Weight: "279 g",
+            SIM: "Dual Sim, Dual Standby (Nano-SIM)",
+            mobile_Color: ["Mystic Black", "Mystic Bronze"],
+        },
+        Processor: {
+            CPU: "Octa-core (1 x 3.09 GHz Kryo 585 + 3 x 2.42 GHz Kryo 585 + 4 x 1.8 GHz Kryo 585)",
+            Chipset: "Qualcomm SM8250 Snapdragon 865+ (7 nm+)",
+            GPU: "Adreno 650",
+        },
+        Display: {
+            Size: "7.6 Inches",
+            Protection: "To be confirmed",
+            Resolution: "1536 x 2208 Pixels (~354 PPI)"
+        },
+        price: "Rs :  269,999",
+
+
+        mobile_Model_2: "Samsung Galaxy Z Flip",
+        Build: {
+            OS: "Android 10 OS",
+            UI: "One UI 2.0",
+            Dimensions: "167.9 x 73.6 x 7.2mm, Folded: (87.4 x 73.6 x 17.3mm)",
+            Weight: "182 g",
+            SIM: "eSIM (Nano-SIM)",
+            mobile_Color: ["Brown", "Mirror Black", "Mirror Purple", "Mirror Gold", "Thom Browne Edition"],
+        },
+        Processor: {
+            CPU: "Octa-core (1 x 3.09 GHz Kryo 585 + 3 x 2.42 GHz Kryo 585 + 4 x 1.8 GHz Kryo 585)",
+            Chipset: "Qualcomm SM8250 Snapdragon 865+ (7 nm+)",
+            GPU: "Adreno 650",
+        },
+        Display: {
+            Size: "6.7 Inches",
+            Protection: "To be confirmed",
+            Resolution: "1536 x 2208 Pixels (~354 PPI)",
+        },
+        price: "Rs : 249,999",
+    },
+
+     iPhone : {
+
+
+        mobile_Model_2: "Apple iPhone 11 Pro Max",
+        Build: {
+            OS: "IOS 13",
+            Dimensions: "157.5 x 77.4 x 8.1 mm",
+            Weight: "226 g",
+            SIM: "Dual Sim, Dual Standby (Nano-SIM)",
+            mobile_Color: ["Space Gray", "Silver", "Gold", "Midnight Green"],
+        },
+        Processor: {
+            Chipset: "Apple A13 (7 nm+)",
+        },
+        Display: {
+            Size: "6.5 Inches",
+            Protection: "To be confirmed",
+            Resolution: "1536 x 2208 Pixels (~354 PPI)",
+        },
+        price: "Rs : 226,600",  
+
+       
+
+        mobile_Model_2: "Apple iPhone 11",
+        Build: {
+            OS: "IOS 13",
+            Dimensions: "157.5 x 77.4 x 8.1 mm",
+            Weight: "226 g",
+            SIM: "Dual Sim, Dual Standby (Nano-SIM)",
+            mobile_Color: ["Space Gray", "Silver", "Gold", "Midnight Green"],
+        },
+        Processor: {
+            Chipset: "Apple A13 (7 nm+)",
+        },
+        Display: {
+            Size: "6.5 Inches",
+            Protection: "To be confirmed",
+            Resolution: "1536 x 2208 Pixels (~354 PPI)",
+        },
+        price: "Rs : 226,600",
+
+     }
+}
+console.log(mobile)
